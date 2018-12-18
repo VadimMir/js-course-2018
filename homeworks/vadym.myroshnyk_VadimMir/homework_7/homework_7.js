5822dd57bf700a5f76d8d2d591cd6d61d9860c03
VadimMir <Miroshnik_vy@ukr.net>
2018-12-18 14:50:25 +0200

    up2

--- homeworks/vadym.myroshnyk_VadimMir/homework_7/homework_7.js
+++ homeworks/vadym.myroshnyk_VadimMir/homework_7/homework_7.js
@@ -1,17 +1,19 @@
 // fibonacci
-const random = Math.floor(Math.random() * (100 - 50 + 1) + 50);
+const randomNum = Math.floor(Math.random() * (100 - 50 + 1) + 50);
 
 function fib(random) {
-   let a = 1;
-   let b = 1;
-   for (let i = 3; i <= random; i++ ){
-       let c = a + b;
-       a = b;
-       b = c;
-   }
-   return b;
+    const arr = [];
+    let a = 1;
+    let b = 0;
+    for (let i = 0; i <= random; i += a) {
+        const c = a + b;
+        a = b;
+        b = c;
+        arr.push(c);
+    }
+    return arr;
 }
 
-console.log(fib(random));
+console.log(fib(randomNum));
 
 module.exports = fib;
